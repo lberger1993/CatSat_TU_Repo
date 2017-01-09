002ebe6724f8dd10454ab8d84c9dfb7665f27099
LuciaEve <luciaeve@wlan-141-23-160-167.tubit.tu-berlin.de>
2017-01-09 17:10:54 +0100

    Adds pinout with testing

--- tmpLM35_sensor/tmpLM35_sensor.ino
+++ tmpLM35_sensor/tmpLM35_sensor.ino
@@ -1,21 +1,27 @@
-int val;
-int tempPin = 1;
+const int sensor=A1; // Assigning analog pin A1 to variable 'sensor'
+
+float tempc;  
+float tempf;  
+float vout; 
 
 void setup()
 {
-Serial.begin(9600);
-}
-void loop()
-{
-val = analogRead(tempPin);
-float mv = ( val/1024.0)*5000; 
-float cel = mv/10;
-float farh = (cel*9)/5 + 32;
 
-Serial.print("TEMPRATURE = ");
-Serial.print(cel);
-Serial.print("*C");
+pinMode(sensor,INPUT); // Configuring pin A1 as input
+Serial.begin(9600);
+
+}
+void loop() 
+{
+
+vout=analogRead(sensor);
+vout=(vout*500)/1023;
+tempc=vout; // Storing value in Degree Celsius
+
+Serial.print("in DegreeC=");
+Serial.print("\t");
+Serial.print(tempc);
 Serial.println();
-delay(1000);
+delay(1000); //Delay of 1 second for ease of viewing 
 
 }
